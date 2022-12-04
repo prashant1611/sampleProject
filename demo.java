87615195261b10f866e1ed25f674b5753d15024d
prashant kumar <prashantplg30@gmail.com>
2022-12-04 23:10:03 +0530

    i am adding a new function

--- demo.java
+++ demo.java
@@ -2,4 +2,7 @@ class simple{
     public static void main(String[] args) {
         System.out.println("hy my name is simple");
     }
+    public static void print(){
+        System.out.println("hi i am adding new function");
+    }
 }
